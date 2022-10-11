7d92f446210d615e9da93458d095dbb2cc990007
sivasakthi <sivasivasakthi56789@gmail.com>
2022-10-11 16:09:58 +0530

    first commit

---
+++ infix.py
@@ -0,0 +1,26 @@
+operation=set(['+','-','*','/','(',')','^'])
+priority={'+':1,'-':1,'*':2,'/':2,'^':3}
+def infinixtopostfix(expression):
+    stack=[]
+    output=''
+    for character in expression:
+        if character not in operation:
+            output += character
+        elif character=='(':
+            stack.append('(')
+        elif character==')':
+            while stack and stack[-1]!='(':
+                output+=stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1]!='(' and priority[character]<=priority[stack[-1]]:
+                output+=stack.pop()
+                stack.append(character)
+        while stack:
+            output+=stack.pop()
+        return output
+
+expression=input("enter infix expression:")
+print('infix notation:',expression)
+print('postfix notation:',infinixtopostfix(expression))
+               
